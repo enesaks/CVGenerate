70a745c3f7f3338fcf72a0ad891eb7a719e2ab33
Enes <98012557+enesaks@users.noreply.github.com>
2025-05-03 00:43:16 +0300

    InfoLayout ve CvPage düzenlendi

---
+++ CVGenerateUI/Controllers/TemplateSelectionController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CVGenerateUI.Controllers
+{
+    public class TemplateSelectionController : Controller
+    {
+        // GET: TemplateSelectionController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
